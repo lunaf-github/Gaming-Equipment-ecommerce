d062073942190de0b157db4e02de7d5311bdcd9c
lunaf-github <lunaf91@gmail.com>
2022-11-28 19:36:10 -0800

    add shoe jpg

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,5 +36,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+    postcss:null,
+  },
+
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <app-textlockup />
+    <app-textlockup>
+      <template v-slot:img>
+        <img src = "/bag.jpg" alt="bag" />
+      </template>
+      <template v-slot:new>60%</template>
+      <template v-slot:sale>Storewide Sale</template>
+      <template v-slot:collection>Summer</template>
+      <template v-slot:details>All accessories</template>
+    </app-textlockup>
   </div>
 </template>
 
