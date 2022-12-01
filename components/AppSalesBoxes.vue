4fa663cb183c89f7c5b702db65dfb2bc5690532e
lunaf-github <lunaf91@gmail.com>
2022-11-30 21:41:34 -0800

    complete sale boxes

--- components/AppSalesBoxes.vue
+++ components/AppSalesBoxes.vue
@@ -37,7 +37,51 @@ export default {
 <style lang="scss" scoped>
 .boxes {
  margin: 70px 0;
- border: 1px #ddd;
- min-height: 150px;
+ div {
+  border: 1px solid #ddd;
+  min-height: 150px;
+  justify-self: center;
+  align-self: center;
+  text-align: center;
+ }
+ .box1{
+  grid-area: 1 / 1 / 2 / 2;
+ }
+
+ .box2{
+  grid-area: 1 / 2 / 2 / 3;
+ }
+
+ .box3{
+  grid-area: 1 / 3 / 2 / 4;
+ }
+}
+
+h4{
+  text-transform: uppercase;
+  letter-spacing: 0.15em;
+  margin: 0 0 10px 0;
+}
+ @media screen and (min-width: 700px) {
+  .boxes {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr;
+    grid-column-gap: 20px;
+    grid-row-gap: 0px;
+    div {
+      padding: 40px 50px;
+    }
+  }
+}
+@media screen and (max-width: 699px) {
+  .boxes {
+    width: 83vw;
+    margin-left: 5vw;
+    div {
+      padding: 10px 20px;
+      margin-bottom: 10px;
+    }
+  }
 }
 </style>
