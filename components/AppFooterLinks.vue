1710a9c557d113539e7779121c57511bb0582142
lunaf-github <lunaf91@gmail.com>
2022-12-02 13:54:31 -0800

    add footer links

---
+++ components/AppFooterLinks.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <ul>
+      <li>About</li>
+      <li>Company</li>
+      <li>Locations</li>
+      <li>Contact</li>
+      <li>Hours</li>
+    </ul>
+    <ul>
+      <li>Twitter</li>
+      <li>Facebook</li>
+      <li>Instagram</li>
+      <li>LinkedIn</li>
+    </ul>
+    <ul>
+     <h4>Newsletter:</h4>
+     <input type="text" />
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
