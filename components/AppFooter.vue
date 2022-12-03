dd5c2de123d16b528a7304252e18a475fd9ca4c3
lunaf-github <lunaf91@gmail.com>
2022-12-02 22:58:51 -0800

    complete markup

---
+++ components/AppFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+   <app-footer-links />
+   <div class="fernstuff">
+    <p>
+     This project was developed by Fernando Luna. 
+    </p>
+   </div>
+  </footer>
+</template>
+
+<script>
+import AppFooterLinks from "~/components/AppFooterLinks.vue";
+
+export default {
+ components:{
+  AppFooterLinks
+ }
+};
+</script>
+
+<style>
+
+</style>
