658e79e8b04e4353e7a46994601c50ecf6ced2ce
lunaf-github <lunaf91@gmail.com>
2022-12-02 23:14:50 -0800

    add styles to footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -3,7 +3,9 @@
    <app-footer-links />
    <div class="fernstuff">
     <p>
-     This project was developed by Fernando Luna. 
+     This project is 
+     <a href="https://github.com/lunaf-github/Gaming-Equipment-ecommerce">open source</a>
+     and was developed by Fernando Luna. 
     </p>
    </div>
   </footer>
@@ -19,6 +21,19 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+ .fernstuff {
+  background: black;
+  width: 100%;
+  padding: 10px 30px;
+  color: white;
+  text-align: center;
+ }
 
+ a,
+ a:active,
+ a:visited {
+  color:#c4c3ec;
+  font-weight: bold;
+ }
 </style>
