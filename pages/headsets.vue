74e1e42bbdc9014f8b7adc097e51f02b2c7fe981
lunaf-github <lunaf91@gmail.com>
2022-12-07 00:21:53 -0800

    Add headphones page

---
+++ pages/headsets.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["headsets"])
+  }
+}
+</script>
+
+<style>
+
+</style>
