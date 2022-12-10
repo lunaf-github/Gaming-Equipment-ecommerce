150f8dbb1573ab2e3536b21baeb9e8f40a6c19df
lunaf-github <lunaf91@gmail.com>
2022-12-09 20:59:18 -0800

    Create accessories page

---
+++ pages/Accessories.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <app-store-grid :data="accessories" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import AppStoreGrid from '../components/AppStoreGrid.vue';
+export default {
+  computed: {
+    ...mapGetters(["accessories"])
+  },
+  components: {
+    AppStoreGrid
+  }
+}
+</script>
+
+<style>
+
+</style>
