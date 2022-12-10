ae7916e8bf777cd1ac489aaf441de179c4aee9a2
lunaf-github <lunaf91@gmail.com>
2022-12-09 20:41:30 -0800

    Add Desk, Keyboard, Mouse Devices pages

---
+++ pages/Keyboards.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+   <app-store-grid :data="keyboards"/>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex"
+import AppStoreGrid from "~/components/AppStoreGrid.vue"
+
+export default {
+  computed: {
+    ...mapGetters(["keyboards"])
+  },
+  components: {
+    AppStoreGrid
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/MouseDevices.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+   <app-store-grid :data="mouseDevices"/>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import AppStoreGrid from '../components/AppStoreGrid.vue';
+export default {
+ computed:{
+   ...mapGetters(["mouseDevices"])
+ },
+ components: {
+   AppStoreGrid
+ }
+
+}
+</script>
+
+<style>
+
+</style>
