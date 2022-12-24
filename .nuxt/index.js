8abfc3d1ab8197a796a8f3d1cc81cdfc202d4002
lunaf-github <lunaf91@gmail.com>
2022-12-24 13:24:49 -0800

    .nuxt updates

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -14,6 +14,7 @@ import { createStore } from './store.js'
 /* Plugins */
 
 import nuxt_plugin_plugin_be0ab224 from 'nuxt_plugin_plugin_be0ab224' // Source: ./components/plugin.js (mode: 'all')
+import nuxt_plugin_currencyfilter_e0c2928e from 'nuxt_plugin_currencyfilter_e0c2928e' // Source: ../plugins/currency-filter.js (mode: 'all')
 
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
@@ -212,6 +213,10 @@ async function createApp(ssrContext, config = {}) {
 
   if (typeof nuxt_plugin_plugin_be0ab224 === 'function') {
     await nuxt_plugin_plugin_be0ab224(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_currencyfilter_e0c2928e === 'function') {
+    await nuxt_plugin_currencyfilter_e0c2928e(app.context, inject)
   }
 
   // Lock enablePreview in context
--- nuxt.config.js
+++ nuxt.config.js
@@ -37,8 +37,7 @@ export default {
   css: ['normalize.css', { src: '~/assets/main.scss', lang: 'sass' }],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: ['~/plugins/currency-filter.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
