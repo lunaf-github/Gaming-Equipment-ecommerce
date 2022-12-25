07d7ab1c63ca3eb79a06584a2aaec8f750386576
lunaf-github <lunaf91@gmail.com>
2022-12-24 23:42:56 -0800

    update dist static files for netlify deployment

---
+++ .nuxt/dist/server/components/app-sales-boxes.js
@@ -0,0 +1,123 @@
+exports.ids = [5];
+exports.modules = {
+
+/***/ 61:
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(69);
+if(content.__esModule) content = content.default;
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add CSS to SSR context
+var add = __webpack_require__(4).default
+module.exports.__inject__ = function (context) {
+  add("5d14e659", content, true, context)
+};
+
+/***/ }),
+
+/***/ 68:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_AppSalesBoxes_vue_vue_type_style_index_0_id_29ba55ef_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(61);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_AppSalesBoxes_vue_vue_type_style_index_0_id_29ba55ef_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_AppSalesBoxes_vue_vue_type_style_index_0_id_29ba55ef_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_AppSalesBoxes_vue_vue_type_style_index_0_id_29ba55ef_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_AppSalesBoxes_vue_vue_type_style_index_0_id_29ba55ef_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+
+
+/***/ }),
+
+/***/ 69:
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(3);
+var ___CSS_LOADER_EXPORT___ = ___CSS_LOADER_API_IMPORT___(false);
+// Module
+___CSS_LOADER_EXPORT___.push([module.i, ".boxes[data-v-29ba55ef]{margin:70px 0}.boxes div[data-v-29ba55ef]{border:1px solid #ddd;min-height:150px;justify-self:center;align-self:center;text-align:center}.boxes .box1[data-v-29ba55ef]{grid-area:1/1/2/2}.boxes .box2[data-v-29ba55ef]{grid-area:1/2/2/3}.boxes .box3[data-v-29ba55ef]{grid-area:1/3/2/4}h4[data-v-29ba55ef]{text-transform:uppercase;letter-spacing:.15em;margin:0 0 10px 0}@media screen and (min-width: 700px){.boxes[data-v-29ba55ef]{display:grid;grid-template-columns:1fr 1fr 1fr;grid-template-rows:1fr;grid-column-gap:20px;grid-row-gap:0px}.boxes div[data-v-29ba55ef]{padding:40px 50px}}@media screen and (max-width: 699px){.boxes[data-v-29ba55ef]{width:83vw;margin-left:5vw}.boxes div[data-v-29ba55ef]{padding:10px 20px;margin-bottom:10px}}", ""]);
+// Exports
+module.exports = ___CSS_LOADER_EXPORT___;
+
+
+/***/ }),
+
+/***/ 73:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/AppSalesBoxes.vue?vue&type=template&id=29ba55ef&scoped=true&
+var render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('section', {
+    staticClass: "boxes"
+  }, [_vm._ssrNode(_vm._ssrList(_vm.boxes, function (box, i) {
+    return "<div" + _vm._ssrClass(null, `box${i}`) + " data-v-29ba55ef><img" + _vm._ssrAttr("src", box.icon) + " alt=\"service icon\" data-v-29ba55ef> <h4 data-v-29ba55ef>" + _vm._ssrEscape(_vm._s(box.heading)) + "</h4> <p data-v-29ba55ef>" + _vm._ssrEscape(_vm._s(box.details)) + "</p></div>";
+  }))]);
+};
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./components/AppSalesBoxes.vue?vue&type=template&id=29ba55ef&scoped=true&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/AppSalesBoxes.vue?vue&type=script&lang=js&
+/* harmony default export */ var AppSalesBoxesvue_type_script_lang_js_ = ({
+  data() {
+    return {
+      boxes: [{
+        icon: "/icon-package.svg",
+        heading: "Free Shipping Globally",
+        details: "Delivery in 4 Days"
+      }, {
+        icon: "/icon-cal.svg",
+        heading: "24/7 Customer Service",
+        details: "Call us any time"
+      }, {
+        icon: "/icon-service.svg",
+        heading: "Easy Online Returns",
+        details: "Send Within 30 Days"
+      }]
+    };
+  }
+});
+// CONCATENATED MODULE: ./components/AppSalesBoxes.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_AppSalesBoxesvue_type_script_lang_js_ = (AppSalesBoxesvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./components/AppSalesBoxes.vue
+
+
+
+function injectStyles (context) {
+  
+  var style0 = __webpack_require__(68)
+if (style0.__inject__) style0.__inject__(context)
+
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_AppSalesBoxesvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  "29ba55ef",
+  "6fae5633"
+  
+)
+
+/* harmony default export */ var AppSalesBoxes = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=app-sales-boxes.js.map
