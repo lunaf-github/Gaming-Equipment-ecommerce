90bcd56986351749d5a96f062c3fc32bf37ce935
lunaf-github <lunaf91@gmail.com>
2023-02-01 22:30:46 -0800

    rename css using BEM

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -1,38 +1,38 @@
 <template>
-  <header>
-   <h1>Gamer's Den</h1>
-   <nav>
-    <ul>
-     <li>
-      <nuxt-link to="/">Home</nuxt-link>
+  <header class="header">
+   <h1 class="header__logo">Gamer's Den</h1>
+   <nav class="nav">
+    <ul class="nav__menu-items">
+     <li class="nav__item">
+      <nuxt-link class="nav__item-link" to="/">Home</nuxt-link>
      </li>
-     <li>
-      <nuxt-link to="/All">All</nuxt-link>
+     <li class="nav__item">
+      <nuxt-link class="nav__item-link" to="/All">All</nuxt-link>
      </li>
-     <li>
-      <nuxt-link to="/Accessories">Accessories</nuxt-link>
+     <li class="nav__item">
+      <nuxt-link class="nav__item-link" to="/Accessories">Accessories</nuxt-link>
      </li>
-     <li>
-      <nuxt-link to="/Desks">Desks</nuxt-link>
+     <li class="nav__item">
+      <nuxt-link class="nav__item-link" to="/Desks">Desks</nuxt-link>
      </li>
-     <li>
-      <nuxt-link to="/Chairs">Chairs</nuxt-link>
+     <li class="nav__item">
+      <nuxt-link class="nav__item-link" to="/Chairs">Chairs</nuxt-link>
      </li>
-     <li>
-      <nuxt-link to="/keyboards">Keyboards</nuxt-link>
+     <li class="nav__item">
+      <nuxt-link class="nav__item-link" to="/keyboards">Keyboards</nuxt-link>
      </li>
-     <li>
-      <nuxt-link to="/MouseDevices">Mouse Devices</nuxt-link>
+     <li class="nav__item">
+      <nuxt-link class="nav__item-link" to="/MouseDevices">Mouse Devices</nuxt-link>
      </li>
-     <li>
-      <nuxt-link to="/headsets">Headsets</nuxt-link>
+     <li class="nav__item">
+      <nuxt-link class="nav__item-link" to="/headsets">Headsets</nuxt-link>
      </li>
-     <li>
-      <nuxt-link to="/Monitors">Monitors</nuxt-link>
+     <li class="nav__item">
+      <nuxt-link class="nav__item-link" to="/Monitors">Monitors</nuxt-link>
      </li>
-     <li>
-      <div class="carttotal" v-if="cartCount > 0">{{ cartCount }}</div>
-      <nuxt-link to="/Cart">Cart</nuxt-link>
+     <li class="nav__item">
+      <div class="nav__cart-total" v-if="cartCount > 0">{{ cartCount }}</div>
+      <nuxt-link class="nav__item-link" to="/Cart">Cart</nuxt-link>
      </li>
     </ul>
    </nav>
@@ -50,53 +50,53 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  header {
+  .header {
     display:flex;
     justify-content: center;
     flex-direction: column;
     padding: 40px 40px 0;
   }
 
-  nav {
+  .nav {
     display: flex;
     width: 80vw;
     margin-top: 30px;
     justify-content: center;
     align-items: center;
     padding: 8px 0;
-    ul {
+    &__menu-items {
       padding-left: 0;
-      li {
-        display: inline;
-        margin-left: 1vw;
-        text-transform: uppercase;
-        letter-spacing: 0.1em;
-        font-size: .9em;
-        position: relative;
-        a {
-          color: white;
-          font-weight: bold;
-          &:hover {
-            color: #c14103;
-          }
-        }
+    }
+    &__item {
+      display: inline;
+      margin-left: 1vw;
+      text-transform: uppercase;
+      letter-spacing: 0.1em;
+      font-size: .9em;
+      position: relative;
+    }
+    &__item-link {
+      color: white;
+      font-weight: bold;
+      &:hover {
+        color: #c14103;
       }
+    }
+    &__cart-total {
+      position:absolute;
+      border-radius: 100%;
+      background-color: white;
+      color:black;
+      font-size: 10px;
+      top: -23px;
+      right: -15px;
+      width: 6px;
+      height: 14px;
+      padding: 6px 10px;
+      font-size: 10px;
+      font-weight: bold;
+      text-align:center;
     }
   }
 
-  .carttotal {
-    position:absolute;
-    border-radius: 100%;
-    background-color: black;
-    color:white;
-    font-size: 10px;
-    top: -23px;
-    right: -15px;
-    width: 6px;
-    height: 14px;
-    padding: 6px 10px;
-    font-size: 10px;
-    font-weight: bold;
-    text-align:center;
-  }
 </style>
